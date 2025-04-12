805ec42da220afde127f17ad3a9ccfa676c1c26d
Official-WaveGaming <mathias_welle@gmx.de>
2025-04-13 01:32:04 +0200

    edit

---
+++ src/main/java/wavebooster/WaveBooster.java
@@ -0,0 +1,17 @@
+package wavebooster;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WaveBooster extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
